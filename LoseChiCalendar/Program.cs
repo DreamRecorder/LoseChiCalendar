51c74b1c08f1bfeef31d1960ff76addc27bec6f0
Wencey Wang <wencey@outlook.com>
2019-05-30 09:17:47 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/LoseChiCalendar;C173

---
+++ LoseChiCalendar/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using DreamRecorder.ToolBox.General;
+using HtmlAgilityPack;
+
+namespace LoseChiCalendar
+{
+
+    /// <summary>
+    /// 黄欣然用来找工作的玄学指导老黄历
+    /// </summary>
+    class Program
+    {
+        private static List<string> ThingsTodo { get; set; } = new List<string>() {"a", "b", "c"};
+
+        private static List<string> PlaceToGo { get; set; } = new List<string>() {"a", "b", "c"};
+
+
+        static async Task Main(string[] args)
+        {
+            
+        }
+    }
+
+
+}
