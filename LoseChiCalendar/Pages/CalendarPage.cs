b949ffdc4216556077bcb70c50c26f930db8713f
Wencey Wang <wencey@outlook.com>
2019-06-11 08:23:06 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/LoseChiCalendar;C200

--- LoseChiCalendar/Pages/CalendarPage.cs
+++ LoseChiCalendar/Pages/CalendarPage.cs
@@ -1,6 +1,7 @@
 ﻿using System ;
 using System . Collections ;
 using System . Collections . Generic ;
+using System . Globalization ;
 using System . Linq ;
 using System . Xml . Linq ;
 
@@ -25,7 +26,7 @@ namespace LoseChiCalendar . Pages
 			DateLabel = Find<FIGletLabel>(nameof(DateLabel));
 			DayNameLabel= Find<Label>(nameof(DayNameLabel));
 			ExitButton = Find <Button> ( nameof ( ExitButton ) ) ;
-			MonthCalendarContainer=Find<Container>(nameof(MonthCalendarContainer))
+			MonthCalendarContainer = Find <Container> ( nameof ( MonthCalendarContainer ) ) ;
 
             ExitButton.Pressed += ExitButton_Pressed;
 		}
@@ -43,6 +44,13 @@ namespace LoseChiCalendar . Pages
 		public Button ExitButton { get ; set ; }
 
 		public Container MonthCalendarContainer { get; set; }
+
+		public DateTime? MonthCalendarDate
+		{
+			get => MonthCalendarContainer?.Tag as DateTime?;
+            set => MonthCalendarContainer.Tag = value;
+		}
+
 
 		public DateTime CurrentDateTime
 		{
@@ -63,75 +71,94 @@ namespace LoseChiCalendar . Pages
 			DateLabel . Text = CurrentDateTime. Day . ToString ( ) ;
 			DayNameLabel . Text = CurrentDateTime. ToString ( "dddd" ) ;
 
-			Container.
+			if ( MonthCalendarDate?.Date != CurrentDateTime.Date)
+			{
+				CultureInfo cultureInfo = new CultureInfo("en-US");
 
-            var canvas = new Canvas();
+                DateTimeFormatInfo dateTimeFormat = cultureInfo.DateTimeFormat;
 
-            panel.Items.Add(canvas);
+				Canvas canvas = new Canvas();
 
-            string[] dayNames = dtfi.AbbreviatedDayNames;
+				MonthCalendarContainer.Content=(canvas);
 
+				MonthCalendarDate = CurrentDateTime . Date ;
 
-            for (int i = 0; i < 7; i++)
-            {
-                Label label = new Label()
-                {
-                    Text = (dayNames[i]),
-                    HorizontalAlign = ContentHorizontalAlign.Left,
-                    Width = 3,
-                };
+                int y = 0 ;
 
-                if (i == 0 || i == 6)
-                {
-                    label.ForegroundColor = ConsoleColor.DarkRed;
-                }
+                string[] dayNames = dateTimeFormat.AbbreviatedDayNames;
 
-                canvas.Items.Add(label);
+				for (int i = 0; i < 7; i++)
+				{
+					Label label = new Label()
+								{
+									Text            = (dayNames[i]),
+									HorizontalAlign = ContentHorizontalAlign.Left,
+									Width           = 3,
+								};
 
-                canvas[label] = new Point((6 * i) + 1, y);
+					if (i == 0 || i == 6)
+					{
+						label.ForegroundColor = ConsoleColor.DarkRed;
+					}
+
+					canvas.Items.Add(label);
+
+					canvas[label] = new Point((6 * i) + 1, y);
+				}
+
+				y++;
+
+                DateTime firstMonthDay = new DateTime(CurrentDateTime.Year, CurrentDateTime.Month, 1);
+
+                int daysInMonth = DateTime.DaysInMonth(CurrentDateTime.Year, CurrentDateTime.Month);
+
+				int weekday = (int)firstMonthDay.DayOfWeek;
+
+				for (int i = 1; i <= daysInMonth; i++)
+				{
+					Button button = new Button
+									{
+										Name            = $"button{i}",
+										Text            = $"{i}",
+										HorizontalAlign = ContentHorizontalAlign.Right,
+										Width           = 5,
+									};
+
+					if (i < 11)
+					{
+						button.KeyBind = i.ToString().Last();
+					}
+
+					if (weekday == 0 || weekday == 6)
+					{
+						button.ForegroundColor = ConsoleColor.Red;
+					}
+
+					if ( i== CurrentDateTime.Day)
+					{
+						button.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    if (new DateTime(CurrentDateTime.Year, CurrentDateTime.Month, i) == DateTime.Today.Date)
+					{
+						button.ForegroundColor = ConsoleColor.Blue;
+					}
+
+					canvas.Items.Add(button);
+
+					canvas[button] = new Point(6 * weekday, y);
+
+					weekday++;
+
+					while (weekday >= 7)
+					{
+						weekday -= 7;
+						y++;
+					}
+				}
             }
 
-            y++;
 
-            int weekday = (int)firstMonthDay.DayOfWeek;
-
-            for (int i = 1; i <= daysInMonth; i++)
-            {
-                Button button = new Button
-                {
-                    Name = $"button{i}",
-                    Text = $"{i}",
-                    HorizontalAlign = ContentHorizontalAlign.Right,
-                    Width = 5,
-                };
-
-                if (i < 11)
-                {
-                    button.KeyBind = i.ToString().Last();
-                }
-
-                if (weekday == 0 || weekday == 6)
-                {
-                    button.ForegroundColor = ConsoleColor.Red;
-                }
-
-                if (i == today)
-                {
-                    button.ForegroundColor = ConsoleColor.Blue;
-                }
-
-                canvas.Items.Add(button);
-
-                canvas[button] = new Point(6 * weekday, y);
-
-                weekday++;
-
-                while (weekday >= 7)
-                {
-                    weekday -= 7;
-                    y++;
-                }
-            }
         }
 
         public override void OnNavigateTo()
